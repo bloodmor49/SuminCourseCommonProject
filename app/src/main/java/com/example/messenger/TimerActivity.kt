1928741e14548a91c19c31af7eec6ac85a3f9224
Bloodmor49 <spacemorine@yandex.ru>
2022-03-12 21:06:44 +0300

    Cамопроверка материала.

--- app/src/main/java/com/example/messenger/TimerActivity.kt
+++ app/src/main/java/com/example/messenger/TimerActivity.kt
@@ -2,41 +2,36 @@ package com.example.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.TextView
 import java.util.*
-import android.os.Handler
-import android.os.PersistableBundle
-import kotlinx.coroutines.Runnable
 
 class TimerActivity : AppCompatActivity() {
 
-    private var seconds:Int = 0
-    //переменная для контроля работы таймера. (Триггер)
-    private var isRunning:Boolean = false
-
-    private var wasRunning: Boolean = false
-
-    private lateinit var timerTextView: TextView
+    private var seconds = 0
+    private var isRunning = false
+    private var wasRunning = false
+    private lateinit var textViewTimer : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
-        timerTextView = findViewById(R.id.TextViewTimer)
+        textViewTimer = findViewById(R.id.TextViewTimer)
+
         runTimer()
     }
 
-    //Сохранение данных при повороте экрана - происходит изменение конфигурации OnDestroied
+    //сохранение и переинициализация данных при уничтожении активити (например - поворот экрана)
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         with(outState) {
-            putInt("seconds", seconds)
-            putBoolean("isRunning", isRunning)
-            putBoolean("wasRunning", wasRunning)
+            putInt("seconds",seconds)
+            putBoolean("isRunning",isRunning)
+            putBoolean("isRunning",wasRunning)
         }
     }
-
-    //Пересоздается экран с перезаписью всех переменных
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         seconds = savedInstanceState.getInt("seconds")
@@ -44,61 +39,49 @@ class TimerActivity : AppCompatActivity() {
         wasRunning = savedInstanceState.getBoolean("wasRunning")
     }
 
-    /* Не нужно, заменяется onPause, onResume в соответствии с условиями задачи.
-    override fun onStop() {
-        super.onStop()
-        wasRunning = isRunning
-        isRunning = false
-    }
-
-    override fun onStart() {
-        super.onStart()
-        isRunning = wasRunning
-    }
-*/
+    //работа при сбитии фокуса
     override fun onPause() {
         super.onPause()
         wasRunning = isRunning
         isRunning = false
     }
-
     override fun onResume() {
         super.onResume()
         isRunning = wasRunning
     }
 
-
-
+    //кнопки
     fun startTimer(view: View) {
         isRunning = true
     }
     fun stopTimer(view: View) {
+        wasRunning = isRunning
         isRunning = false
     }
     fun resetTimer(view: View) {
         isRunning = false
-        seconds = 0
+        textViewTimer.text = getString(R.string.timerInit)
     }
+
+    //Handler - фоновый поток
     private fun runTimer(){
-        //Создаем объект класса Handler
-        val timeHandler = Handler()
-        //далее запускаем отсчет времени прямо сейчас, создавая анонимный класс Runnable
-        timeHandler.post(object: Runnable {
-            //стандартная функция запуска процесса run класса Runnable
+        var timerHandler = Handler()
+        timerHandler.post(object :Runnable {
             override fun run() {
                 var hours = seconds / 3600
                 var minutes = (seconds % 3600) / 60
                 var secs = seconds % 60
                 var time = String.format( "%d:%02d:%02d", hours, minutes, secs)
-                timerTextView.text = time
+                textViewTimer.text = time
                 if (isRunning) seconds++
 
-                //при выполнении задания ждем минуту и снова запускаем анонимный класс
-
-                timeHandler.postDelayed(this,1000)
+                timerHandler.postDelayed(this,1000)
             }
-        }
-        )
+        })
     }
 }
+
+
+
+
 
