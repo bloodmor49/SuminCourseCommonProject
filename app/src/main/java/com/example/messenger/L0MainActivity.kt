b436674dbb015929ce377d265135b8a51014f1f4
Bloodmor49 <spacemorine@yandex.ru>
2022-03-18 23:14:08 +0300

    6 урок - работа с htmlURLConnection, json

--- app/src/main/java/com/example/messenger/L0MainActivity.kt
+++ app/src/main/java/com/example/messenger/L0MainActivity.kt
@@ -3,33 +3,30 @@ package com.example.messenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class L0MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        actionBar?.show()
 
-    fun lesson1intent(view: View) {
-        intent = Intent(this,L1MainColorSpinnerActivity::class.java)
-        startActivity(intent)
-    }
-    fun lesson2intent(view: View) {
-        intent = Intent(this,L2MainCreateMessageActivity::class.java)
-        startActivity(intent)
-    }
-    fun lesson3intent(view: View) {
-        intent = Intent(this,L3MainTimerActivity::class.java)
-        startActivity(intent)
-    }
-    fun lesson4intent(view: View) {
-        intent = Intent(this,L4MainLoginActivity::class.java)
-        startActivity(intent)
-    }
-    fun lesson5intent(view: View) {
-        intent = Intent(this,L5ShopMainActivity::class.java)
-        startActivity(intent)
-    }
+        var mainListView = findViewById<ListView>(R.id.mainListView)
 
+        mainListView.setOnItemClickListener { parent, view, position, id ->
+
+            when (position) {
+
+                0 -> startActivity(Intent(this,L1MainColorSpinnerActivity::class.java))
+                1 -> startActivity(Intent(this,L2MainCreateMessageActivity::class.java))
+                2 -> startActivity(Intent(this,L3MainTimerActivity::class.java))
+                3 -> startActivity(Intent(this,L4MainLoginActivity::class.java))
+                4 -> startActivity(Intent(this,L5ShopMainActivity::class.java))
+                5 -> startActivity(Intent(this,L6JSONProjectActivity::class.java))
+
+            }
+
+        }
+    }
 }
